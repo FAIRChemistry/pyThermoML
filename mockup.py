7ad5a8f556613db5e9c59f8e5880376ab1522385
Jan Range <jan.range@simtech.uni-stuttgart.de>
2021-06-29 14:22:55 +0200

    Mockup vision

--- mockup.py
+++ mockup.py
@@ -4,9 +4,48 @@ Project: pyThermoML
 Author: Jan Range
 License: BSD-2 clause
 -----
-Last Modified: Tuesday June 29th 2021 12:20:00 pm
+Last Modified: Tuesday June 29th 2021 2:22:39 pm
 Modified By: Jan Range (<jan.range@simtech.uni-stuttgart.de>)
 -----
 Copyright (c) 2021 Institute of Biochemistry and Technical Biochemistry Stuttgart
 '''
+
+# Initialize dataReport parent class
+dataRep = DataReport(
+    "Hallo",
+    "DOI",
+    "Name"
+)
+
+# Define compounds
+comp1 = Compound()
+comp2 = Compound()
+
+comp1_ID = dataRep.addCompound(comp1)
+comp2_ID = dataRep.addCompound(comp2)
+
+# Initialize PureMixtureData
+experiment1 = PureMixtureData()
+
+# Define properties
+prop1 = peakTemperature('ID')
+prop2 = viscocity('ID2')
+
+# Define Variables
+var1 = temperature('ID3')
+
+prop1_ID = experiment1.addProperty()
+prop2_ID = experiment1.addProperty()
+var1_ID = experiment1.addVariable()
+
+# Measurement
+
+def generateData():
+    return data
+
+meas = Measurement(
+    prop1_ID=10.0,
+    prop2_ID=20.0,
+    var1_ID=20.0
+)
 
